e7ddccc412ad31ae3288dfc63a79610a3cacd295
Haesung Park <hea9549@gmail.com>
2019-07-18 16:22:27 +0900

    feat: initial package structure

---
+++ src/component/CenterView/CenterView.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./MainHeader.scss";
+/* interface IProps {
+    text: string;
+    done: boolean;
+    onToggle(): void;
+    onRemove(): void;
+}
+*/
+
+export default class MainHeader extends React.Component {
+    public render(): React.ReactNode {
+        return (
+            <div id="HeaderBox">
+                sadf
+            </div>
+        );
+
+    }
+}
